c8c5fdcbaea413f964c5d00b31bc335a33c9a91a
Phó Tiến Anh <85491773+photienanh@users.noreply.github.com>
2022-11-18 18:54:11 +0700

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 #include<string>
 using namespace std;
-char space[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+char board[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
 //create render
 void render(){
      cout<<"-------------"<<endl;
               for(int i=0;i<3;i++){
                 cout<<"| ";
                 for(int j=0;j<3;j++){
-                    cout<<space[i][j]<<" | ";
+                    cout<<board[i][j]<<" | ";
                 }
                 cout<<endl<<"-------------"<<endl;
                 
@@ -19,8 +19,8 @@ char nextPlayer(){
     int countX=0,countO=0;
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            if(space[i][j]=='X')countX++;
-            else if(space[i][j]=='O')countO++; 
+            if(board[i][j]=='X')countX++;
+            else if(board[i][j]=='O')countO++; 
         }
     }
     if(countX==countO) return 'X';
@@ -31,31 +31,31 @@ int checkEnd(){
     /*X win*/
     //Row
     for(int i=0;i<3;i++){
-        if(space[i][0]==space[i][1]&&space[i][0]==space[i][2]&&space[i][0]=='X') return 1;
+        if(board[i][0]==board[i][1]&&board[i][0]==board[i][2]&&board[i][0]=='X') return 1;
     }
     //Collumn
     for(int j=0;j<3;j++){
-        if(space[0][j]==space[1][j]&&space[0][j]==space[2][j]&&space[0][j]=='X') return 1;
+        if(board[0][j]==board[1][j]&&board[0][j]==board[2][j]&&board[0][j]=='X') return 1;
     }
     //Cross
-    if(space[0][0]==space[1][1]&&space[0][0]==space[2][2]&&space[0][0]=='X') return 1;
-    if(space[0][2]==space[1][1]&&space[0][2]==space[2][0]&&space[2][0]=='X') return 1;
+    if(board[0][0]==board[1][1]&&board[0][0]==board[2][2]&&board[0][0]=='X') return 1;
+    if(board[0][2]==board[1][1]&&board[0][2]==board[2][0]&&board[2][0]=='X') return 1;
     /*O win*/
     //Row
     for(int i=0;i<3;i++){
-        if(space[i][0]==space[i][1]&&space[i][0]==space[i][2]&&space[i][0]=='O') return 2;
+        if(board[i][0]==board[i][1]&&board[i][0]==board[i][2]&&board[i][0]=='O') return 2;
     }
     //Collumn
     for(int j=0;j<3;j++){
-        if(space[0][j]==space[1][j]&&space[0][j]==space[2][j]&&space[0][j]=='O') return 2;
+        if(board[0][j]==board[1][j]&&board[0][j]==board[2][j]&&board[0][j]=='O') return 2;
     }
     //Cross
-    if(space[0][0]==space[1][1]&&space[0][0]==space[2][2]&&space[0][0]=='O') return 2;
-    if(space[0][2]==space[1][1]&&space[0][2]==space[2][0]&&space[2][0]=='O') return 2;
+    if(board[0][0]==board[1][1]&&board[0][0]==board[2][2]&&board[0][0]=='O') return 2;
+    if(board[0][2]==board[1][1]&&board[0][2]==board[2][0]&&board[2][0]=='O') return 2;
     /*not end*/
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            if(space[i][j]>='1' && space[i][j]<='9') return -1;
+            if(board[i][j]>='1' && board[i][j]<='9') return -1;
         }
     }
     return 0;//draw
@@ -64,9 +64,9 @@ int checkEnd(){
 void playgame(char n){
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            if(space[i][j]==n){
-                if(nextPlayer()=='X') space[i][j]='X';
-                else if(nextPlayer()=='O') space[i][j]='O';
+            if(board[i][j]==n){
+                if(nextPlayer()=='X') board[i][j]='X';
+                else if(nextPlayer()=='O') board[i][j]='O';
             }
         }
     }
@@ -88,24 +88,36 @@ int main(){
         if(nextPlayer()=='O') cout<<"It's "<<name2<<"'s turn, please choose your move: ";
         cin>>n;
         if(n<'1'||n>'9') {
-            cout<<"invalid";
-            break;
+            cout<<"invalid"<<endl;
+            playgame(n);
+            render();
+            if(checkEnd()==1){
+                cout<<name1<<" win";
+                return 0;
+            }
+            if(checkEnd()==2){
+                cout<<name2<<" win";
+                return 0;
+            }
+            if(checkEnd()==0){
+                cout<<"A draw";
+                return 0;
+            }
         }
-        else{
-        playgame(n);
-        render();
-        if(checkEnd()==1){
-            cout<<name1<<" win";
-            return 0;
-        }
-        if(checkEnd()==2){
-            cout<<name2<<" win";
-            return 0;
-        }
-        if(checkEnd()==0){
-            cout<<"A draw";
-            return 0;
-        }
+        else{ playgame(n);
+            render();
+            if(checkEnd()==1){
+                cout<<name1<<" win";
+                return 0;
+            }
+            if(checkEnd()==2){
+                cout<<name2<<" win";
+                return 0;
+            }
+            if(checkEnd()==0){
+                cout<<"A draw";
+                return 0;
+            }
         }
     }
     
